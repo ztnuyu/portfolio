8fba201abc1e1138a9a5096b06ef8c6076ed524a
ztnuyu <izzatisuhaimi@icloud.com>
2021-12-01 00:21:47 +0800

    first comm

--- izzati/deploy.sh
+++ izzati/deploy.sh
@@ -7,6 +7,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:pragmatic-reviews/portfolio/izzati.git master:gh-pages
+git push -f git@github.com:ztnuyu/portfolio.git master:gh-pages
 
 cd -
